7cb408cf3e4beeb2ca7379e7c8a80d320752b150
Vedansh Saha <74900340+VedanshS@users.noreply.github.com>
2024-04-11 13:48:58 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -16,6 +16,14 @@ navigator.mediaDevices.getUserMedia({
     video:false
 }).then((stream)=>{
     addVideoStream(myVideo,stream)
+
+    peer.on("call",(call)=>{
+            call.answer("stream");
+            const video = document.createElement("video");
+            call.on("stream",(userVideoStream)=>{
+                addVideoStream(video,userVideoStream);
+            });
+    });
 })
 
 function addVideoStream(video, stream) {
